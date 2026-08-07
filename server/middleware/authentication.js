827dcd42ead0b114b23094d506e7d3d0e282c58e
awaiskamil-dev <kamilawais@gmail.com>
2026-08-07 17:17:33 +0500

    Refactoring authenticateUser middleware

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,18 +1,36 @@
 const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
+const { isTokenValid, attachCookiesToResponse } = require('../utils');
+const Token = require('../models/Token');
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  const {refreshToken, accessToken} = req.signedCookies;
 
-  if (!token) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
-  }
+  try{
+    if(accessToken){
+      const payload = isTokenValid(accessToken);
+      req.user = payload.user;
+      return next();
+    }
+    const payload = isTokenValid(refreshToken);
 
-  try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken: payload.refreshToken
+    });
+
+    if(!existingToken || !existingToken.isValid){
+      throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    }
+
+    attachCookiesToResponse({
+      res,
+      user: payload.user,
+      refreshToken: payload.refreshToken
+    });
+
+    req.user = payload.user;
     next();
-  } catch (error) {
+  }catch(error){
     throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
 };
