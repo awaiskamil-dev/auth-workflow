d5b12576de1b6d38a9264bc805a28772c40b826b
awaiskamil-dev <kamilawais@gmail.com>
2026-08-07 17:05:20 +0500

    Creating Token model, refactoring attachCookiesToResponse util, and setting up token in login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require('../utils');
@@ -35,7 +36,7 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const verificationToken = 'fakeToken';
+  const verificationToken = crypto.randomBytes(40).toString('hex');
 
   const user = await User.create({ name, email, password, role, verificationToken });
 
@@ -65,16 +66,34 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
-
   if(!user.isVerified){
     throw new CustomError.UnauthenticatedError('Email is not verified');
   }
   
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
+  let refreshToken = '';
+  const existingToken = await Token.findOne({user: user._id});
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+  if(existingToken){
+    const {isValid} = existingToken;
+    if(!isValid){
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({res, user: tokenUser, refreshToken});
+    res.status(StatusCodes.OK).json({user: tokenUser});
+  }  
+
+  refreshToken = crypto.randomBytes(40).toString('hex');
+  const userAgent = req.headers['user-agent'];
+  const ip = req.ip;
+  const userToken = {refreshToken, ip, userAgent, user: user._id};
+
+  await Token.create(userToken);
+  attachCookiesToResponse({res, user: tokenUser, refreshToken});
+  res.status(StatusCodes.OK).json({user: tokenUser});
 };
+
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
